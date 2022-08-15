7205e4ab6adf4fc8cea267268883d4348527ae40
mperleo <tiomakelelle@outlook.com>
2022-08-15 14:33:23 +0200

    fix to the style and layout to the page on all devices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'mykiDashboard';
+export class AppComponent implements OnInit {
+
+  private scrHeight: number = window.innerHeight;
+  private scrWidth: number = window.innerWidth;
+
+  public atitleClass: string = '';
+  public titleClass: string = '';
+
+  ngOnInit(): void {
+    if(this.scrHeight > this.scrWidth){
+      this.atitleClass='no-decoration';
+      this.titleClass = 'satine';
+    }
+    else{
+      this.atitleClass='text-vertical no-decoration';
+      this.titleClass = 'satine display-1';
+    }
+  }
 }
