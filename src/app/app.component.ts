77b7282f75fef5b484e4f39f0e2179ff7f286878
mperleo <tiomakelelle@outlook.com>
2022-10-05 14:14:20 +0200

    layout changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component, HostListener, OnInit } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent implements OnInit {
+  public activeTab = 'about';
+
   ngOnInit(): void {}
+
+  public onLinkClicked(tabName : string){
+    this.activeTab = tabName;
+  }
 }
