72c06e86a7a45eefa7a4fb4256b17578c4534f7f
mperleo <tiomakelelle@outlook.com>
2022-10-05 12:15:40 +0200

    new redesign of all the layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,21 +6,5 @@ import { Component, HostListener, OnInit } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent implements OnInit {
-
-  private scrHeight: number = window.innerHeight;
-  private scrWidth: number = window.innerWidth;
-
-  public atitleClass: string = '';
-  public titleClass: string = '';
-
-  ngOnInit(): void {
-    if(this.scrHeight > this.scrWidth){
-      this.atitleClass='no-decoration';
-      this.titleClass = 'satine';
-    }
-    else{
-      this.atitleClass='text-vertical no-decoration';
-      this.titleClass = 'satine display-1';
-    }
-  }
+  ngOnInit(): void {}
 }
