96d25296f14cf5f02d7748cd8c72e0b56e814362
mperleo <tiomakelelle@outlook.com>
2023-08-02 10:03:40 +0200

    (chore): reduce size

--- src/app/views/proyects/myki-proyects/myki-proyects.component.ts
+++ src/app/views/proyects/myki-proyects/myki-proyects.component.ts
@@ -12,38 +12,20 @@ export class MykiProyectsComponent implements OnInit {
     {
       name: 'Costos',
       url: '',
-      img: 'assets/img/costos_banner.png',
+      img: 'assets/img/costos_banner.jpeg',
       description: 'Aplicación de fiananzas personales',
-    },
-    {
-      name: 'Droppper',
-      url: '',
-      img: 'assets/img/droppper_banner.png',
-      description: 'Aplicación para subir ficheros a mi servidor',
     },
     {
       name: 'Bike Speed Calculator',
       url: 'https://mperleo.github.io/BikeSpeedCalculator/',
-      img: 'assets/img/bikeSpeed_banner.png',
-      description: 'Calcula el avance y velocidad de pedalada de tu transmisión',
+      img: 'assets/img/bikeSpeed_banner.jpeg',
+      description: 'Calculadora de avance y velocidad de pedalada',
     },
     {
-      name: 'Bingo! Angular',
+      name: 'Bingo!',
       url: 'https://mperleo.github.io/BingoAngular/',
-      img: 'assets/img/bingo_banner.png',
-      description: 'Bingo online con generador de cartones, version creada con Angular',
-    },
-    {
-      name: 'Panaper',
-      url: '',
-      img: 'assets/img/panaper_banner.png',
-      description: 'Web de empresa',
-    },
-    {
-      name: 'Tester',
-      url: '',
-      img: 'assets/img/tester_banner.png',
-      description: 'Aplicación para hacer examenes tipo test',
+      img: 'assets/img/bingo_banner.jpeg',
+      description: 'Simulador de bingo online',
     },
   ];
 
